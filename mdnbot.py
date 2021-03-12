0e9bdcabe29d037fdba5eb249d698dbc5df4eb9f
SvH <mail@seyffenstein-bajar.de>
2021-03-12 17:35:19 +0100

    added facts about nations (part 1 of ?)

--- mdnbot.py
+++ mdnbot.py
@@ -23,6 +23,116 @@ async def on_ready():
 
 #some facts about different nations
 @bot.command()
+async def astor(ctx):
+    embed = discord.Embed(title="Fakten über Astor")
+    embed.add_field(name="Name des Staates", value="United States of Astor")
+    embed.add_field(name="Staatsform", value="präsidiale Republik")
+    embed.add_field(name="Staatsoberhaupt", value="Sarah Jones")
+    embed.add_field(name="Einwohner", value="107.767.700")
+    embed.add_field(name="Kontinent", value="Astor")
+    embed.add_field(name="Hauptstadt", value="Astoria")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def albernia(ctx):
+    embed = discord.Embed(title="Fakten über Albernia")
+    embed.add_field(name="Name des Staates", value="Kingdom of Albernia")
+    embed.add_field(name="Staatsform", value="parlamentarische Monarchie")
+    embed.add_field(name="Staatsoberhaupt", value="Jane II.")
+    embed.add_field(name="Einwohner", value="75.000.000")
+    embed.add_field(name="Kontinent", value="Antica")
+    embed.add_field(name="Hauptstadt", value="Aldenroth")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def alsztyna(ctx):
+    embed = discord.Embed(title="Fakten über Alsztyna")
+    embed.add_field(name="Name des Staates", value="Freie Hansestadt Alsztyna")
+    embed.add_field(name="Staatsform", value="Demokratie")
+    embed.add_field(name="Staatsoberhaupt", value="Jouwe MacDubs")
+    embed.add_field(name="Einwohner", value="715.143")
+    embed.add_field(name="Kontinent", value="Harnar")
+    embed.add_field(name="Hauptstadt", value="Alsztyna-Stadt")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def aquatropolis(ctx):
+    embed = discord.Embed(title="Fakten über Aquatropolis")
+    embed.add_field(name="Name des Staates", value="Seereich Aquatropolis")
+    embed.add_field(name="Staatsform", value="Diktatur")
+    embed.add_field(name="Staatsoberhaupt", value="Jeanne Duchamp")
+    embed.add_field(name="Einwohner", value="1.800.000")
+    embed.add_field(name="Kontinent", value="")
+    embed.add_field(name="Hauptstadt", value="Aquatropolis City")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def andro(ctx):
+    embed = discord.Embed(title="Fakten über Andro")
+    embed.add_field(name="Name des Staates", value="Föderale Republik Andro")
+    embed.add_field(name="Staatsform", value="föderale Demokratie")
+    embed.add_field(name="Staatsoberhaupt", value="Georgi Reingoldowitsch Raschnikow")
+    embed.add_field(name="Einwohner", value="142.754.206")
+    embed.add_field(name="Kontinent", value="Renzia")
+    embed.add_field(name="Hauptstadt", value="Koskow")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def barnstorvia(ctx):
+    embed = discord.Embed(title="Fakten über Barnstorvia")
+    embed.add_field(name="Name des Staates", value="Royaume de Barnstorvia")
+    embed.add_field(name="Staatsform", value="konst. Monarchie")
+    embed.add_field(name="Staatsoberhaupt", value="Louis XXI.")
+    embed.add_field(name="Einwohner", value="142.754.206")
+    embed.add_field(name="Kontinent", value="Renzia")
+    embed.add_field(name="Hauptstadt", value="Brissac")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def bengali(ctx):
+    embed = discord.Embed(title="Fakten über Bengali")
+    embed.add_field(name="Name des Staates", value="Bengali")
+    embed.add_field(name="Staatsform", value="absolute Monarchie")
+    embed.add_field(name="Staatsoberhaupt", value="Muawatalli Aliwata")
+    embed.add_field(name="Einwohner", value="")
+    embed.add_field(name="Kontinent", value="Salvagiti")
+    embed.add_field(name="Hauptstadt", value="Radeshasa")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def bergen(ctx):
+    embed = discord.Embed(title="Fakten über Bergen")
+    embed.add_field(name="Name des Staates", value="Bergen")
+    embed.add_field(name="Staatsform", value="parlamentarische Republik")
+    embed.add_field(name="Staatsoberhaupt", value="Willem Stroh")
+    embed.add_field(name="Einwohner", value="25.061.321")
+    embed.add_field(name="Kontinent", value="Antica")
+    embed.add_field(name="Hauptstadt", value="Freie Stadt Bergen")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def chinopien(ctx):
+    embed = discord.Embed(title="Fakten über Chinopien")
+    embed.add_field(name="Name des Staates", value="Kaiserreich Chinopien")
+    embed.add_field(name="Staatsform", value="absolute Monarchie")
+    embed.add_field(name="Staatsoberhaupt", value="Te Mai")
+    embed.add_field(name="Einwohner", value="657.420.400")
+    embed.add_field(name="Kontinent", value="Renzia")
+    embed.add_field(name="Hauptstadt", value="Qianlongjjing")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def cranberra(ctx):
+    embed = discord.Embed(title="Fakten über Cranberra")
+    embed.add_field(name="Name des Staates", value="Dominion of Cranberra")
+    embed.add_field(name="Staatsform", value="parl. Monarchie")
+    embed.add_field(name="Staatsoberhaupt", value="Jane II.")
+    embed.add_field(name="Einwohner", value="10.300.000")
+    embed.add_field(name="Kontinent", value="Astor")
+    embed.add_field(name="Hauptstadt", value="Oustburgh")
+    await ctx.send(embed=embed)
+
+@bot.command()
 async def nordhanar(ctx):
     embed = discord.Embed(title="Fakten über Nordhanar")
     embed.add_field(name="Name des Staates", value = "Vereinigtes Kaiserthum von Nordhanar")
