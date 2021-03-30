0a18572a9fdb8f54642ea8c25da0d8cd6e167bdb
SvH <bene@fell-it.at>
2021-03-30 22:32:49 +0200

    fixed typo

--- botstart.sh
+++ botstart.sh
@@ -2,7 +2,7 @@
 echo "Ziehe Änderungen aus dem Github-Repo"
 git pull
 echo "Aktiviere VENV"
-source /venv/bin/activate
+source venv/bin/activate
 echo "Starte Bot"
 nohup python3 mdnbot.py
 echo "Bot gestartet"
